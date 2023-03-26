b148b79c873fe0a55947a29c0efc46d075de230f
Lucas <lgxm3z@gmail.com>
2023-03-26 16:06:28 -0300

    add files

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Gomes</title>
+        <meta name="description" content="Welcome" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <main >
+        <h1>Hello World</h1>
+      </main>
+    </>
+  )
+}
